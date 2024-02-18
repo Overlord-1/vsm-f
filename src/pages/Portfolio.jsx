fb1991f5fb4b21f4f825b7c25bb425931a82495f
Overlord <ruchirkhare12@gmail.com>
2024-02-18 15:49:05 +0530

    vTest

---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,68 @@
+import Header from "../components/Header";
+import PLtracker from "../components/PLtracker";
+import Button from "../components/Button";
+import MiddleRow from "../components/MiddleRow";
+import Card from "../components/Card";
+function Portfolio() {
+  // random data
+  // just name the JSON file "data" and all cards would be rendered
+  const data = [
+    {
+      name: "HDFC",
+      avg: 93.87,
+      quantity: 3,
+      LTP: 81.25,
+      profit: 434,
+    },
+    {
+      name: "ONGC",
+      avg: 93.87,
+      quantity: 3,
+      LTP: 81.25,
+      profit: 434,
+    },
+    {
+      name: "JIOFIN",
+      avg: 93.87,
+      quantity: 14,
+      LTP: 81.25,
+      profit: 434,
+    },
+    {
+      name: "HDFC",
+      avg: 93.87,
+      quantity: 3,
+      LTP: 81.25,
+      profit: 434,
+    },
+    {
+      name: "INFY",
+      avg: 1293.87,
+      quantity: 3,
+      LTP: 81.25,
+      profit: -151,
+    }
+  ];
+  return (
+    <>
+      <Header text={"Portfolio"} />
+      <PLtracker
+        invested={"2894.54"}
+        current={"2782.3"}
+        profit={"-239"}
+        perChg={"-4.5"}
+      />
+      <Button
+        text={"View all stocks"}
+        txtColor={"#ffffff"}
+        bgColor={"#343434"}
+      />
+      <MiddleRow />
+      {data.map(parameter=>(
+        <Card name={parameter.name} avg={parameter.avg} quantity={parameter.quantity} LTP={parameter.LTP} profit={parameter.profit} />
+      ))}  
+     </>
+  );
+}
+
+export default Portfolio;
