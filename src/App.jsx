6ade70065c1b56c0b8a64ebc7ff1a7d79d98cf89
Ruchir Khare <ruchirkhare12@gmail.com>
2024-03-08 23:00:26 +0530

    add and beautify CalcRound page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect } from 'react'
+import React, {  } from 'react'
 import Portfolio from './pages/Portfolio'
 import Leaderboard from './pages/Leaderboard'
 import News from './pages/News'
 import Login from './pages/Login'
-import io from 'socket.io-client' 
+// import io from 'socket.io-client' 
 import { Route, Routes } from 'react-router'
 import Loading from './pages/Loading'
+import CalcRound from './pages/CalcRound'
 
 
 
@@ -22,6 +23,7 @@ const App = () => {
       <Route path='/news' element= {<News />}/>
       <Route path='/portfolio' element= {<Portfolio />}/>
       <Route path='/loading' element= {<Loading />}/>
+      <Route path='/calc' element= {<CalcRound />}/>
     </Routes>
     </>
   )
